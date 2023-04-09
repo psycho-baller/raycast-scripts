3bcb4855cd9db333728a67d17991ec7e427da65d
Rami Maalouf <rami.rami@ucalgary.ca>
2023-04-09 07:19:33 -0400

    added danas excel

--- dana.py
+++ dana.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
 # Load the Excel file
-df = pd.read_excel('input_file.xlsx')
+file_path = "input_file.xlsx"
+df = pd.read_excel(file_path)
 
-# Find all negative numbers in the DataFrame and replace them with "ND"
-df[df < 0] = "ND"
+# Loop through all cells in the DataFrame
+for index, row in df.iterrows():
+    for column_name in df.columns:
+        value = row[column_name]
+        # Check if the value is numeric
+        if pd.api.types.is_numeric_dtype(pd.Series(value)):
+            # Replace negative numbers with "ND"
+            if value < 0:
+                df.at[index, column_name] = "ND"
 
-# Save the modified file
-df.to_excel('output_file.xlsx', index=False)
+# Save the modified DataFrame back to the Excel file
+df.to_excel("output_file.xlsx", index=False)
+
+print("Negative numbers in all columns have been replaced with 'ND'.")
